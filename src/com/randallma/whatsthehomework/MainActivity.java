e51cbecf79bd9f884dcc96a88bf05e96311cdad8
Randall Ma <randall@randallma.com>
2012-12-23 21:10:11 -0800

    authentication with flask server working

--- src/com/randallma/whatsthehomework/MainActivity.java
+++ src/com/randallma/whatsthehomework/MainActivity.java
@@ -1,6 +1,7 @@
 package com.randallma.whatsthehomework;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 
@@ -10,6 +11,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+//		 check if logged in
+	
+		Intent myIntent = new Intent(this, LoginActivity.class);
+		startActivity(myIntent);
+		
 	}
 	
 	@Override
