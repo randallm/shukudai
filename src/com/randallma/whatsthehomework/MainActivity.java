924289aee285d53403e9188ae290ebdc560e7c03
Randall Ma <randall@randallma.com>
2012-12-28 21:35:08 -0800

    json parsed and put into hashmap

--- src/com/randallma/whatsthehomework/MainActivity.java
+++ src/com/randallma/whatsthehomework/MainActivity.java
@@ -1,5 +1,11 @@
 package com.randallma.whatsthehomework;
 
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +15,7 @@ import android.widget.TextView;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.PersistentCookieStore;
 
 public class MainActivity extends Activity {
@@ -60,14 +67,44 @@ public class MainActivity extends Activity {
 					}
 				});
 
-		// int newsFeedPage = 1;
-		// clientSession.post("http://192.168.1.42:5000/hw/news/dummy/all/"
-		// + newsFeedPage + '/', new JsonHttpResponseHandler() {
-		// @Override
-		// public void onSuccess(JSONArray response) {
-		// System.out.println(response);
-		// }
-		// });
+		clientSession.get("http://192.168.1.42:5000/news/dummy/all/",
+				new JsonHttpResponseHandler() {
+					@Override
+					public void onSuccess(JSONObject response) {
+
+						JSONArray assignments = null;
+						try {
+							assignments = response.getJSONArray("assignments");
+							JSONObject c = assignments.getJSONObject(0);
+							for (int i = 0; i < assignments.length() + 1; i++) {
+
+								JSONObject individualAssignment = new JSONObject(
+										c.getString(Integer.toString(i)));
+
+								String photo = individualAssignment
+										.getString("photo");
+								String dateAssigned = individualAssignment
+										.getString("date_assigned");
+								String dateDue = individualAssignment
+										.getString("date_due");
+								String description = individualAssignment
+										.getString("description");
+
+								HashMap<String, String> map = new HashMap<String, String>();
+								map.put("photo", photo);
+								map.put("dateAssigned", dateAssigned);
+								map.put("dateDue", dateDue);
+								map.put("description", description);
+
+								System.out.println(map);
+							}
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+					}
+				});
 
 	}
 
