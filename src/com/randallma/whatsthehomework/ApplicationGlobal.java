f4497f8e2b3def9b799de97526a1a3db39574456
Randall Ma <randall@randallma.com>
2012-12-29 21:47:25 -0800

    JSON populates MainActivity "news feed"
    currently only dummy JSON

--- src/com/randallma/whatsthehomework/ApplicationGlobal.java
+++ src/com/randallma/whatsthehomework/ApplicationGlobal.java
@@ -1,14 +1,22 @@
 package com.randallma.whatsthehomework;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.app.Application;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.PersistentCookieStore;
 
 public class ApplicationGlobal extends Application {
+	// var declarations
+
 	private AsyncHttpClient clientSession = null;
 	private PersistentCookieStore cookieStore = null;
 	public boolean userLoggedIn = false;
+	public ArrayList<HashMap<String, String>> newsFeed = new ArrayList<HashMap<String, String>>();
+
+	// get methods
 
 	public AsyncHttpClient getClientSession() {
 		return clientSession;
@@ -22,15 +30,25 @@ public class ApplicationGlobal extends Application {
 		return userLoggedIn;
 	}
 
-	public void setClientSession(AsyncHttpClient c) {
-		this.clientSession = c;
+	public ArrayList<HashMap<String, String>> getNewsFeed() {
+		return newsFeed;
 	}
 
-	public void setCookieStore(PersistentCookieStore c) {
-		this.cookieStore = c;
+	// set methods
+
+	public void setClientSession(AsyncHttpClient x) {
+		this.clientSession = x;
 	}
 
-	public void setUserLoggedIn(boolean s) {
-		this.userLoggedIn = s;
+	public void setCookieStore(PersistentCookieStore x) {
+		this.cookieStore = x;
+	}
+
+	public void setUserLoggedIn(boolean x) {
+		this.userLoggedIn = x;
+	}
+
+	public void setNewsFeed(ArrayList<HashMap<String, String>> newsList) {
+		this.newsFeed = newsList;
 	}
 }
--- src/com/randallma/whatsthehomework/MainActivity.java
+++ src/com/randallma/whatsthehomework/MainActivity.java
@@ -1,24 +1,27 @@
 package com.randallma.whatsthehomework;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.SimpleAdapter;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.PersistentCookieStore;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
+
+	SimpleAdapter adapter;
 
 	private void login() {
 		ApplicationGlobal g = (ApplicationGlobal) getApplication();
@@ -45,67 +48,91 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		ApplicationGlobal g = (ApplicationGlobal) getApplication();
+
 		boolean userLoggedIn = g.getUserLoggedIn();
+		AsyncHttpClient clientSession = new AsyncHttpClient();
+		PersistentCookieStore cookieStore = g.getCookieStore();
+		clientSession.setCookieStore(cookieStore);
 		if (!userLoggedIn) {
 			login();
 		} else {
-			AsyncHttpClient clientSession = new AsyncHttpClient();
-			PersistentCookieStore cookieStore = g.getCookieStore();
-			clientSession.setCookieStore(cookieStore);
 
-			clientSession.get("http://192.168.1.42:5000/motd/",
-					new AsyncHttpResponseHandler() {
-						@Override
-						public void onSuccess(String response) {
-							// bad session in cookie fallthrough
-							if (response.equals("False")) {
-								login();
-							}
-							TextView motd = (TextView) findViewById(R.id.motd);
-							motd.setText("Welcome, " + response + ".");
-						}
-					});
+			SimpleAdapter adapter = new SimpleAdapter(
+					this,
+					newsFeed,
+					R.layout.assignment_list_row_view,
+					new String[] { "photo", "dateAssigned", "dateDue",
+							"description" },
+					new int[] { R.id.photo, R.id.dateAssigned, R.id.dateDue, R.id.description });
 
-			clientSession.get("http://192.168.1.42:5000/news/dummy/all/",
-					new JsonHttpResponseHandler() {
-						@Override
-						public void onSuccess(JSONObject response) {
+			getNewsJson();
 
-							JSONArray assignments = null;
-							try {
-								assignments = response
-										.getJSONArray("assignments");
-								JSONObject c = assignments.getJSONObject(0);
-								for (int i = 0; i < assignments.length() + 1; i++) {
-
-									JSONObject individualAssignment = new JSONObject(
-											c.getString(Integer.toString(i)));
-
-									String photo = individualAssignment
-											.getString("photo");
-									String dateAssigned = individualAssignment
-											.getString("date_assigned");
-									String dateDue = individualAssignment
-											.getString("date_due");
-									String description = individualAssignment
-											.getString("description");
-
-									HashMap<String, String> map = new HashMap<String, String>();
-									map.put("photo", photo);
-									map.put("dateAssigned", dateAssigned);
-									map.put("dateDue", dateDue);
-									map.put("description", description);
-
-									System.out.println(map);
-								}
-							} catch (JSONException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-
-						}
-					});
 		}
+	}
+
+	private SimpleAdapter initAdapter(
+			ArrayList<HashMap<String, String>> newsFeed) {
+		SimpleAdapter adapter = new SimpleAdapter(this, newsFeed,
+				R.layout.assignment_list_row_view, new String[] { "photo",
+						"dateAssigned", "dateDue", "description" }, new int[] {
+						R.id.photo, R.id.dateAssigned, R.id.dateDue, R.id.description });
+		return adapter;
+	}
+
+	ArrayList<HashMap<String, String>> newsFeed = new ArrayList<HashMap<String, String>>();
+
+	private void getNewsJson() {
+		ApplicationGlobal g = (ApplicationGlobal) getApplication();
+		AsyncHttpClient clientSession = new AsyncHttpClient();
+		PersistentCookieStore cookieStore = g.getCookieStore();
+		clientSession.setCookieStore(cookieStore);
+		clientSession.get("http://192.168.1.42:5000/news/dummy/all/",
+				new JsonHttpResponseHandler() {
+					@Override
+					public void onSuccess(JSONObject response) {
+						JSONArray assignments = new JSONArray();
+						try {
+							assignments = response.getJSONArray("assignments");
+							JSONObject c = assignments.getJSONObject(0);
+
+							SimpleAdapter adapter = initAdapter(newsFeed);
+
+							for (int i = 0; i < assignments.length() + 1; i++) {
+
+								JSONObject individualAssignment = new JSONObject(
+										c.getString(Integer.toString(i)));
+
+								HashMap<String, String> map = new HashMap<String, String>();
+								map.put("photo",
+										individualAssignment.getString("photo"));
+								map.put("dateAssigned", individualAssignment
+										.getString("date_assigned"));
+								map.put("dateDue", individualAssignment
+										.getString("date_due"));
+								map.put("description", individualAssignment
+										.getString("description"));
+
+								newsFeed.add(map);
+
+								if (i == 0) {
+									adapter.notifyDataSetChanged();
+									setListAdapter(adapter);
+								} else {
+									adapter.notifyDataSetChanged();
+								}
+							}
+
+						} catch (JSONException e) {
+							e.printStackTrace();
+						}
+					}
+
+					@Override
+					public void onFailure(Throwable e, String response) {
+						// TODO: catch network errors (if fail, don't clear news
+						// feed)
+					}
+				});
 	}
 
 	@Override
