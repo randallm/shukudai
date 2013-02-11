f60d4ee071f704ed50bd019b0cfa81aa160b582e
Randall Ma <randall@randallma.com>
2013-02-10 17:09:18 -0800

    increment default due date by 1 day

--- src/com/randallma/whatsthehomework/DatePickerFragment.java
+++ src/com/randallma/whatsthehomework/DatePickerFragment.java
@@ -16,7 +16,8 @@ public class DatePickerFragment extends DialogFragment implements
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		final Calendar c = Calendar.getInstance();
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.DATE, 1);
 		int year = c.get(Calendar.YEAR);
 		int month = c.get(Calendar.MONTH);
 		int day = c.get(Calendar.DAY_OF_MONTH);
