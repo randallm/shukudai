9b61cdfa48c943195482eb8f00b4c55b8da2db1f
Randall Ma <randall@randallma.com>
2013-04-08 22:26:52 -0700

    add AboutActivity into activity hierarchy

--- src/com/randallma/whatsthehomework/AboutActivity.java
+++ src/com/randallma/whatsthehomework/AboutActivity.java
@@ -1,16 +1,19 @@
 package com.randallma.whatsthehomework;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class AboutActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
 
@@ -23,8 +26,26 @@ public class AboutActivity extends Activity {
 	}
 
 	@Override
+	public void onBackPressed() {
+		Intent mainActivityIntent = new Intent(AboutActivity.this,
+				MainActivity.class);
+		mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+		startActivity(mainActivityIntent);
+		finish();
+	};
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Intent mainActivityIntent = new Intent(AboutActivity.this,
+				MainActivity.class);
+		mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+		startActivity(mainActivityIntent);
+		finish();
+		return true;
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.about, menu);
 		return true;
 	}
